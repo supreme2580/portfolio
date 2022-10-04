9bba39c17ac5e8f5fd59ee828e3a73ebecedf02b
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-04 08:58:03 +0100

    suprememods

---
+++ components/Header.tsx
@@ -0,0 +1,50 @@
+import { SocialIcon } from "react-social-icons"
+import { motion } from "framer-motion"
+
+export default function Header() {
+    return(
+        <header className="sticky top-0 flex items-start justify-center sm:justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
+            <motion.div
+            initial={{
+                x: -500,
+                opacity: 0,
+                scale: 0.5
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1
+            }}
+            transition={{
+                duration: 1.5
+            }}
+            className="flex flex-row items-center">
+                {/* Social icons */}
+                <SocialIcon url="https://twitter.com/VictorOmorogbe3" fgColor="gray" bgColor="transparent" />
+                <SocialIcon url="http://discordapp.com/users/Supreme2580#2263" bgColor="gray" />
+                <SocialIcon url="https://twitter.com/VictorOmorogbe3" fgColor="gray" bgColor="transparent" />
+                <SocialIcon url="https://twitter.com/VictorOmorogbe3" fgColor="gray" bgColor="transparent" />
+                <SocialIcon url="https://twitter.com/VictorOmorogbe3" fgColor="gray" bgColor="transparent" />
+            </motion.div>
+            <motion.div
+            initial={{
+                x: 500,
+                opacity: 0,
+                scale: 0.5
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1
+            }}
+            transition={{
+                duration: 1.5
+            }}
+            className="flex flex-row items-center text-gray-300 cursor-pointer">
+                {/* Contact me */}
+                <SocialIcon network="email" fgColor="gray" bgColor="transparent" className="cursor-pointer" />
+                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
+            </motion.div>
+        </header>
+    )
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import { Cursor, useTypewriter } from "react-simple-typewriter"
+import BackgroundCircles from "./BackgroundCircles"
+
+export default function Hero() {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, The Name's Victor Omorogbe",
+            "Guy-who-loves-tech.tsx",
+            "<WithAPassionForCoding />",
+            "LetBuildSomething.js"
+        ],
+        loop: true,
+        delaySpeed: 2000
+    })
+    return(
+        <div className="min-h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+            <BackgroundCircles />
+            <Image src={"/profilepic.jpg"} width={125} height={125} className="rounded-full relative mx-auto object-cover" alt="Profile Image" />
+            <div>
+                <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px] sm:tracking-[15px]">Software Engineer</h2>
+                <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-semibold px-5 lg:px-10">
+                    <span className="mr-3">{text}</span>
+                    <Cursor cursorColor="#F7AB0A" />          
+                </h1>             
+            </div>
+        </div>
+    )
+}
