8861b299b9842568ac36b2f53627945fc3991040
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-04 09:23:25 +0100

    suprememods

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
@@ -12,10 +13,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <section id="hero" className='snap-center'>
+      {/* Hero */}
+      <section id="hero" className='snap-start'>
         <Hero />
       </section>
       {/* About */}
+      <section id="about" className='snap-center'>
+        <About />
+      </section>
       {/* Experience */}
       {/* Skills */}
       {/* Projects */}
