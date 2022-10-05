77332aba8732e1e3ef172b0e6b61403ea16234b5
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-05 17:01:33 +0100

    suprememods

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,7 +16,7 @@ export default function Projects() {
                 {/* Projects */}
                 {
                     projects.map((project, index) =>
-                    <div className="flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center py-10 md:p-22 h-auto shadow-2xl my-4">
+                    <div key={Math.random()} className="flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center py-10 md:p-22 h-auto shadow-2xl my-4">
                         <div className="relative mt-8 xl:mt-0">
                             <motion.img
                                 initial={{ y: -300, opacity: 0 }}
