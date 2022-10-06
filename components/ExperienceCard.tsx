9a442f88e3e4553f16b291893f8428e51f4cb526
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-07 00:59:10 +0100

    suprememods

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -24,7 +24,7 @@ interface Props {
 export default function ExperienceCard({ image, title, name, techStack, startDate, endDate }: Props) {
     return(
         <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[300px] sm:w-[500px] md:w-[600px] bg-[#292929]
-            p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden">
+            p-7 sm:p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden">
             <motion.img
                 initial={{
                     y: -100,
@@ -41,22 +41,22 @@ export default function ExperienceCard({ image, title, name, techStack, startDat
                 className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
                 alt=""
                 src={`${urlFor(image)}`} />
-            <div className="px-0 md:px-10 w-full">
-                <h4 className="text-4xl font-light">{title}</h4>
-                <p className="font-bold text-2xl mt-1">{name}</p>
-                <div className="space-x-2 my-2 flex overflow-x-auto w-[300px] scrollbar-hide">
+            <div className="w-full px-0 md:px-10">
+                <h4 className="text-4xl font-light text-center">{title}</h4>
+                <p className="mt-1 text-2xl font-bold">{name}</p>
+                <div className="space-x-2 my-2 flex overflow-x-auto w-[300px] scrollbar-hide pr-12">
                     {/* Tech used */}
                     {
                         techStack.map(
                             item => (
                                 <div className="min-w-[55px] h-14 relative" key={item.asset._ref}>
-                                    <Image src={`${urlFor(item.asset._ref)}`} width={50} height={50} className="rounded-full object-center" layout="fill" />
+                                    <Image src={`${urlFor(item.asset._ref)}`} width={50} height={50} className="object-center rounded-full" layout="fill" />
                                 </div>
                             )
                         )
                     }
                 </div>
-                <p className="uppercase py-5 text-gray-300">{startDate} - {endDate ? endDate : "Till Now"}</p>
+                <p className="py-5 text-gray-300 uppercase">{startDate} - {endDate ? endDate : "Till Now"}</p>
             </div>
         </article>
     )
