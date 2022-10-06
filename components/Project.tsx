29ed834cacef5b95173ef1e7a4da582cccf6c2e5
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-06 21:50:38 +0100

    suprememods

--- components/Project.tsx
+++ components/Project.tsx
@@ -12,13 +12,13 @@ interface Props {
 export default function Project({  image, title, description, index, length }: Props) {
     return(
         <div key={Math.random()} className="flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center py-10 md:p-22 h-auto shadow-2xl my-4">
-                        <div className="relative mt-8 xl:mt-0">
+                        <div className="relative md:px-2.5 lg:px-0">
                             <motion.img
                                 initial={{ y: -300, opacity: 0 }}
                                 transition={{ duration: 1.2 }}
                                 whileInView={{ opacity: 1, y: 0 }}
                                 viewport={{ once: true }}
-                                src={`${urlFor(image)}`} className="w-[285px] md:w-[450px] h-64" />
+                                src={`${urlFor(image)}`} className="w-[285px] md:w-[450px] h-64 md:h-[300px]" />
                         </div>
                         <div className="space-y-2.5 max-w-[300px] lg:max-w-lg">
                             <h4 className="text-lg md:text-3xl xl:text-4xl font-semibold text-center">
