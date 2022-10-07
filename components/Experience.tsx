054cfa565e7743cf4828532db5134d541431b8a4
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-07 02:16:26 +0100

    suprememods

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export default function Experience({ experience }: Props) {
     return(
-        <div className="relative flex flex-col items-center h-full px-5 xl:px-10">
+        <div className="relative flex flex-col items-center h-full xl:px-10">
             <h3 className="uppercase tracking-[10px] text-gray-500 text-2xl mt-20 px-5">Experience</h3>
-            <div className="flex max-w-full p-10 space-x-5 overflow-x-scroll text-left scrollbar-hide">
+            <div className="flex w-full p-10 space-x-5 overflow-x-scroll text-left scrollbar-hide">
                 {
                     experience.map(exp => <ExperienceCard
                         key={exp._id}
